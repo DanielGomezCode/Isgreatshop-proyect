2bc6167aacfe1bcd5289c3d40fbd0e4d2ebbfbde
dani <danigomezlo2022@gmail.com>
2022-12-12 10:43:30 -0500

    inicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,48 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './information/home/home.component';
+import { AboutComponent } from './information/about/about.component';
+import { ShopComponent } from './information/shop/shop.component';
+import { ContactComponent } from './information/contact/contact.component';
+import { TermsComponent } from './information/terms/terms.component';
+import { PrivacyComponent } from './politics/privacy/privacy.component';
+import { ShippingComponent } from './politics/shipping/shipping.component';
+import { RefundComponent } from './politics/refund/refund.component';
+import { TrackOrderComponent } from './politics/track-order/track-order.component';
+import { PaymentComponent } from './politics/payment/payment.component';
+import { HouseComponent } from './categories/house/house.component';
+import { TechnologyComponent } from './categories/technology/technology.component';
+import { PetsComponent } from './categories/pets/pets.component';
+import { BeautyComponent } from './categories/beauty/beauty.component';
+import { FitnessComponent } from './categories/fitness/fitness.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { CartComponent } from './cart/cart.component';
+import { BtnUpComponent } from './btn-up/btn-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ShopComponent,
+    ContactComponent,
+    TermsComponent,
+    PrivacyComponent,
+    ShippingComponent,
+    RefundComponent,
+    TrackOrderComponent,
+    PaymentComponent,
+    HouseComponent,
+    TechnologyComponent,
+    PetsComponent,
+    BeautyComponent,
+    FitnessComponent,
+    HeaderComponent,
+    FooterComponent,
+    CartComponent,
+    BtnUpComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/btn-up/btn-up.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-btn-up',
+  templateUrl: './btn-up.component.html',
+  styleUrls: ['./btn-up.component.sass']
+})
+export class BtnUpComponent {
+
+}
